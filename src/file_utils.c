13954c2232e8ecb3e14728819f8bb2171a67cc82
MoonEgg <kraiwish123@gmail.com>
2023-06-15 09:28:19 +0700

    fix: :bug: fix heredoc

--- src/file_utils.c
+++ src/file_utils.c
@@ -6,7 +6,7 @@
 /*   By: kkaiyawo <kkaiyawo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 11:50:14 by kkaiyawo          #+#    #+#             */
-/*   Updated: 2023/06/08 13:46:13 by kkaiyawo         ###   ########.fr       */
+/*   Updated: 2023/06/15 09:23:54 by kkaiyawo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,16 +50,19 @@ int	file_open(t_fileset *fs, t_parser *ps)
 int	heredoc_open(t_fileset *fs, t_parser *ps)
 {
 	char	*line;
+	char	*name;
 
-	(void) ps;
-	ft_strlcat(fs->name, "\n", ft_strlen(fs->name) + 2);
+	name = malloc(ft_strlen(fs->name) + 2);
+	if (name == NULL)
+		executor_error(ps, NULL, ALLOC_ERROR, errno);
+	ft_strlcpy(name, fs->name, ft_strlen(fs->name) + 1);
+	ft_strlcat(name, "\n", ft_strlen(fs->name) + 2);
 	fs->fd = open("/tmp/heredoc", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	line = NULL;
 	while (1)
 	{
 		write(1, "> ", 2);
 		line = get_next_line(0);
-		if (ft_strncmp(line, fs->name, strlen(fs->name) + 1) == 0)
+		if (ft_strncmp(line, name, strlen(name) + 1) == 0)
 			break ;
 		write(fs->fd, line, strlen(line));
 		if (line != NULL)
@@ -69,6 +72,7 @@ int	heredoc_open(t_fileset *fs, t_parser *ps)
 		free(line);
 	file_close(&fs->fd);
 	fs->fd = open("/tmp/heredoc", O_RDONLY);
+	ft_free(name);
 	return (0);
 }
 
